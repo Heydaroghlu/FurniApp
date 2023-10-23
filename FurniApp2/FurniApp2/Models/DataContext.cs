66ba5f0787792b619ab607b6d7aee2a32edec6b5
Heydaroghlu <heyderovisa777@gmail.com>
2023-10-23 18:44:37 +0400

    send

---
+++ FurniApp2/FurniApp2/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FurniApp2.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+        DbSet<Product> Products { get; set;}
+        public DbSet<User> Users { get; set; }
+
+    }
+}
